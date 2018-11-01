f7f7d947733ac870760d5a5955d5cc92b7f10378
Уринбасаров Диас Нуржанулы <@corp.itstep>
2018-11-01 20:19:46 +0600

    done

---
+++ Serialization/Atribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializeHome
+{
+    class Atribute : Attribute
+    {
+        public string Info { get; set; }
+
+        public Atribute() { }
+
+        public Atribute(string info)
+        {
+            Info = info;
+        }
+    }
+}
